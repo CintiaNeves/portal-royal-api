adf0337aab7f925e84a06a750f67d59a6744a90e
Cintia Neves <maria.cintia@rhfranquias.com.br>
2020-09-09 14:49:11 -0300

    Adiciona arquivo Procfile

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+const app = express();    
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+app.use(cors());
+
+require('./app/controllers/index')(app);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+
+
